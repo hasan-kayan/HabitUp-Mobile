cb2aedd8120a1ca8f3a21dff8c5c8f2c0cd6d4f5
hasan-kayan <hasankayan2000@hotmail.com>
2024-02-04 13:44:21 +0300

    Init

---
+++ HabitUp_Mobile/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Here is my first React Native Application</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
